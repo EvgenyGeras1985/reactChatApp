0166a83c7145b7cd76bc52888d0ca818b70571b6
Gerasjeka1985 <gekgrom@mail.ru>
2024-02-15 00:06:45 +0300

    nav creating branch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import '../../assets/App.scss';
+import "./app.module.scss.css";
 import Navigation from "../Navigation/NavComponent";
 
 class App extends React.Component{
   render(){
-    return <Navigation />
+    return (
+        <>
+          <Navigation />
+          <article className="description">
+            <div>Вас приветствует</div>
+            <div>React</div>
+            <div>Chat</div>
+          </article>
+        </>
+    )
   }
 }
 
--- src/components/Navigation/NavComponent.js
+++ src/components/Navigation/NavComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './nav.module.scss.css';
-
+import ButtonEnter from "../ButtonEnter/buttonEnter";
 class Navigation extends React.Component{
     render(){
         return <nav className="header">
                     <h2>React Chat</h2>
+                    <ButtonEnter></ButtonEnter>
                 </nav>
     }
 }
